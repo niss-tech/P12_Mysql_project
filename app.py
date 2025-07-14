a8865edf42e0631640a26175eb63005aa0abbf8a
nisrine adamo <nisrineadamo1@gmail.com>
2025-07-14 22:23:31 +0200

    Ajout de la configuration Sentry avec gestion via .env

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
 from menu import menu
+from config.logging_config import init_sentry
+
+init_sentry()
 
 
 if __name__ == "__main__":
---
+++ config/logging_config.py
@@ -0,0 +1,12 @@
+import sentry_sdk
+from dotenv import load_dotenv
+import os
+def init_sentry():
+    sentry_sdk.init(
+    dsn=os.getenv("SENTRY_DSN"),
+    # Add data like request headers and IP for users,
+    # see https://docs.sentry.io/platforms/python/data-management/data-collected/ for more info
+    send_default_pii=True,
+)
+    
+
